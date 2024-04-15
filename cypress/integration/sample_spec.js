2cb37c7d468e889c7e33293e95ec7bea86164624
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-04-15 16:39:42 +0200

    cypress

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,10 @@
+// cypress/integration/sample_spec.js
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://example.com');
+      cy.contains('type').click();
+      cy.url().should('include', '/commands/actions');
+      cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com');
+    });
+  });
+  
