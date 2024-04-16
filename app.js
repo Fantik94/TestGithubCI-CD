3ab05046a766d2c6d65989f300825f0a79070511
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-04-16 14:28:28 +0200

    docker in azure + port 3939

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Home, Bienvenue');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3939;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
